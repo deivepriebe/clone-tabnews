2c58224f24b922061c6e66b87155e8c72f96523e
Deivemar Priebe <150304505+deivepriebe@users.noreply.github.com>
2024-02-05 23:51:00 +0000

    adinado scripts `lint:check` e `lint:fix´

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
-Projeto para treinar programação do zero. 
+
+Projeto para treinar programação do zero.
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
-function Home () {
-    return <h1>Nanda meu amor, bora comprar protetor solar para a nossa viagem? 😎</h1>
+function Home() {
+  return (
+    <h1>Nanda meu amor, bora comprar protetor solar para a nossa viagem? 😎</h1>
+  );
 }
-export default Home; 
+
+export default Home;
